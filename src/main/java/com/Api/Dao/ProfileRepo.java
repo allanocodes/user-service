e67ff91f51856b55453abec39c7cdac696cce5f0
allanocodes <ndururiallan92@gmail.com>
2025-08-11 19:13:22 +0300

    changed project structure

--- src/main/java/com/Api/Dao/ProfileRepo.java
+++
@@ -1,7 +0,0 @@
-package com.Api.Dao;
-
-import com.Api.Entity.UserProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileRepo extends JpaRepository<UserProfile,String> {
-}
--- src/main/java/com/Api/Dao/RoleJpa.java
+++
@@ -1,7 +0,0 @@
-package com.Api.Dao;
-
-import com.Api.Entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleJpa extends JpaRepository<Role,Long> {
-}
